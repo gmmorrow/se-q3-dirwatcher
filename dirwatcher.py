e7fcd29a2767521ea8c902cf7440dfaff29a8f94
gabrielle@Gabbys-MacPro.com <gab_morrow@yahoo.com>
2020-10-21 17:52:47 -0400

    magic word function

--- dirwatcher.py
+++ dirwatcher.py
@@ -3,15 +3,39 @@
 Dirwatcher - A long-running program
 """
 
-__author__ = "???"
+__author__ = "Gabrielle, John, Arianna"
 
 import sys
+import time
+import logging
+import os
+import argparse
+import signal
 
 
-def search_for_magic(filename, start_line, magic_string):
-    # Your code here
-    return
 
+main_dict = {}
+logger = logging.getLogger(__name__)
+
+def search_for_magic(ns):
+    try:
+        for file in main_dict:
+            with open(ns.dir + "/" + file, 'r') as f:
+                lines = f.readlines()
+                for index, line in enumerate(lines):
+                    if ns.magicword in line:
+                        if index not in main_dict[file]:
+                            main_dict[file].append(index)
+                            logger.info(
+                                "The magic word, " +
+                                ns.magicword.upper() + " was found on line "
+                                + str(index + 1) + " in " + file)
+                        if ns.magicword not in line:
+                            logger.info(
+                                "The magic word, " +
+                                ns.magicword.upper() + " is not found")
+    except Exception as e:
+        logger.info(e)
 
 def watch_directory(path, magic_string, extension, interval):
     # Your code here
@@ -31,6 +55,27 @@ def signal_handler(sig_num, frame):
 def main(args):
     # Your code here
     return
+
+
+
+
+# def create_parser():
+#     """Create a command line parser object with 2 argument definitions."""
+#     parser = argparse.ArgumentParser(
+#         description="Extracts and alphabetizes baby names from html.")
+#     parser.add_argument(
+#         '-e', help='extension of searching file name')
+#     parser.add_argument(
+#         '-i', help='polling interval program')
+#     parser.add_argument(
+#         'magic', help='summary file that magic text can be found')
+#     parser.add_argument(
+#         'path', help='Director to search')
+#     parser.add_argument('files', help='filename(s) to parse')
+#     return parser
+
+
+
 
 
 if __name__ == '__main__':
